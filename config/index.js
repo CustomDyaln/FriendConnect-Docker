fcec02b8cbd8e1b4a8d1e4ab090d681ce9fbc26c
CustomDyaln <150633771+CustomDyaln@users.noreply.github.com>
2024-04-14 01:15:15 -0400

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,22 @@
 import { Session } from "friend-connect";
+import { http } from 'http';
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`
+    <html>
+      <head>
+        <title>Your Web View</title>
+      </head>
+      <body style="margin: 0; padding: 0;">
+        <iframe width="100%" height="100%" src="https://axocoder.vercel.app/" frameborder="0" allowfullscreen></iframe>
+      </body>
+    </html>`);
+});
+
+server.listen(1000, () => {
+  console.log('Server Online because of Axo Coder ✅!!');
+});
 
 new Session({
 	hostName: "Astra Portal", // The hostname of the server
