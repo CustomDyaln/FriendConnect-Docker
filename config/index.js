738bd13a905c58892bb54abace2d8c7a0475f6ec
CustomDyaln <150633771+CustomDyaln@users.noreply.github.com>
2024-04-13 21:30:44 -0400

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,14 +1,10 @@
 import { Session } from "friend-connect";
-const express = require("express");
+import express from 'express';
 const app = express();
-const port = 10000;
+const port2 = 10000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
 })
 
 new Session({
